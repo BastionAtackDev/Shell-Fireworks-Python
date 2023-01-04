6bd1fb75c9e0b1d91948ca29e4044cc5a04c5d3f
BastionAtackDev <sbypsm@gmail.com>
2023-01-04 20:28:13 +0200

    first commit

---
+++ shell-fireworks.py
@@ -0,0 +1,97 @@
+import os
+from time import sleep
+def printAll():
+    # print(line1)
+    # print(line2)
+    # print(line3)
+    # print(line4)
+    # print(line5)
+    # print(line6)
+    # print(line7)
+    # print(line8)
+    for i in line13:
+        print(i, end=" ")
+    print()
+    for i in line12:
+        print(i, end=" ")
+    print()
+    for i in line11:
+        print(i, end=" ")
+    print()
+    for i in line10:
+        print(i, end=" ")
+    print()
+    for i in line9:
+        print(i, end=" ")
+    print()
+    for i in line8:
+        print(i, end=" ")
+    print()
+    for i in line7:
+        print(i, end=" ")
+    print()
+    for i in line6:
+        print(i, end=" ")
+    print()
+    for i in line5:
+        print(i, end=" ")
+    print()
+    for i in line4:
+        print(i, end=" ")
+    print()
+    for i in line3:
+        print(i, end=" ")
+    print()
+    for i in line2:
+        print(i, end=" ")
+    print()
+    for i in line1:
+        print(i, end=" ")
+    print()
+
+def render():
+    os.system("cls")
+    printAll()
+    sleep(0.1)
+
+while (True):
+    line13 = [' ', ' ', ' ', ' ', ' ', ' ']
+    line12 = [' ', ' ', ' ', ' ', ' ', ' ']
+    line11 = [' ', ' ', ' ', ' ', ' ', ' ']
+    line10 = [' ', ' ', ' ', ' ', ' ', ' ']
+    line9 = [' ', ' ', ' ', ' ', ' ', ' ']
+    line8 = [' ', ' ', ' ', ' ', ' ', ' ']
+    line7 = [' ', ' ', ' ', ' ', ' ', ' ']
+    line6 = [' ', ' ', ' ', ' ', ' ', ' ']
+    line5 = [' ', ' ', ' ', ' ', ' ', ' ']
+    line4 = [' ', ' ', ' ', ' ', ' ', ' ']
+    line3 = [' ', ' ', ' ', ' ', ' ', ' ']
+    line2 = [' ', ' ', ' ', ' ', ' ', ' ']
+    line1 = [' ', ' ', ' ', ' ', ' ', ' ']
+# render()
+    os.system("cls")
+    line1[4] = "/"
+    render()
+    line1[4] = " "
+    line2[4] = "\\"
+    render()
+    line2[4] = " "
+    line3[4] = "/"
+    render()
+    line3[4] = " "
+    line4[4] = "\\"
+    render()
+    line4[4] = " "
+    line5[4] = "/"
+    render()
+    line5[4] = " "
+    line6[4] = "\\"
+    render()
+    line6[4] = " "
+    line7[4] = "/"
+    render()
+    line7[4] = " "
+    line8[4] = "\\"
+    render()
+    line8[4] = "0"
+    render()
