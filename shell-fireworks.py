298becff7a78f4627e7cbebfeb1092bb786824ac
BastionAtackDev <sbypsm@gmail.com>
2023-01-05 09:43:03 +0200

    Update

--- shell-fireworks.py
+++ shell-fireworks.py
@@ -10,43 +10,43 @@ def printAll():
     # print(line7)
     # print(line8)
     for i in line13:
-        print(i, end=" ")
+        print(i, end="")
     print()
     for i in line12:
-        print(i, end=" ")
+        print(i, end="")
     print()
     for i in line11:
-        print(i, end=" ")
+        print(i, end="")
     print()
     for i in line10:
-        print(i, end=" ")
+        print(i, end="")
     print()
     for i in line9:
-        print(i, end=" ")
+        print(i, end="")
     print()
     for i in line8:
-        print(i, end=" ")
+        print(i, end="")
     print()
     for i in line7:
-        print(i, end=" ")
+        print(i, end="")
     print()
     for i in line6:
-        print(i, end=" ")
+        print(i, end="")
     print()
     for i in line5:
-        print(i, end=" ")
+        print(i, end="")
     print()
     for i in line4:
-        print(i, end=" ")
+        print(i, end="")
     print()
     for i in line3:
-        print(i, end=" ")
+        print(i, end="")
     print()
     for i in line2:
-        print(i, end=" ")
+        print(i, end="")
     print()
     for i in line1:
-        print(i, end=" ")
+        print(i, end="")
     print()
 
 def render():
@@ -54,44 +54,110 @@ def render():
     printAll()
     sleep(0.1)
 
+
+line13 = [' ', ' ', ' ', ' ', ' ', ' ']
+line12 = [' ', ' ', ' ', ' ', ' ', ' ']
+line11 = [' ', ' ', ' ', ' ', ' ', ' ']
+line10 = [' ', ' ', ' ', ' ', ' ', ' ']
+line9 = [' ', ' ', ' ', ' ', ' ', ' ']
+line8 = [' ', ' ', ' ', ' ', ' ', ' ']
+line7 = [' ', ' ', ' ', ' ', ' ', ' ']
+line6 = [' ', ' ', ' ', ' ', ' ', ' ']
+line5 = [' ', ' ', ' ', ' ', ' ', ' ']
+line4 = [' ', ' ', ' ', ' ', ' ', ' ']
+line3 = [' ', ' ', ' ', ' ', ' ', ' ']
+line2 = [' ', ' ', ' ', ' ', ' ', ' ']
+line1 = [' ', ' ', ' ', ' ', ' ', ' ']
+# rnder()
 while (True):
-    line13 = [' ', ' ', ' ', ' ', ' ', ' ']
-    line12 = [' ', ' ', ' ', ' ', ' ', ' ']
-    line11 = [' ', ' ', ' ', ' ', ' ', ' ']
-    line10 = [' ', ' ', ' ', ' ', ' ', ' ']
-    line9 = [' ', ' ', ' ', ' ', ' ', ' ']
-    line8 = [' ', ' ', ' ', ' ', ' ', ' ']
-    line7 = [' ', ' ', ' ', ' ', ' ', ' ']
-    line6 = [' ', ' ', ' ', ' ', ' ', ' ']
-    line5 = [' ', ' ', ' ', ' ', ' ', ' ']
-    line4 = [' ', ' ', ' ', ' ', ' ', ' ']
-    line3 = [' ', ' ', ' ', ' ', ' ', ' ']
-    line2 = [' ', ' ', ' ', ' ', ' ', ' ']
-    line1 = [' ', ' ', ' ', ' ', ' ', ' ']
-# render()
     os.system("cls")
-    line1[4] = "/"
+    line1[2] = '\033[32m'+"/"
     render()
+    line1[2] = " "
+    line2[2] = "\\"
+    render()
+    line2[2] = " "
+    line3[2] = "/"
+    render()
+    line3[2] = " "
+    line4[2] = "\\"
+    render()
+    line4[2] = " "
+    line5[2] = "/"
+    line1[4] = "\\"
+    render()
+    line5[2] = " "
+    line6[2] = "\\"
     line1[4] = " "
-    line2[4] = "\\"
+    line2[4] = "/"
     render()
+    line6[2] = " "
+    line7[2] = "/"
     line2[4] = " "
-    line3[4] = "/"
+    line3[4] = "\\"
     render()
+    line7[2] = " "
+    line8[2] = "\\"
     line3[4] = " "
-    line4[4] = "\\"
+    line4[4] = "/"
     render()
+    line8[2] = "0"
     line4[4] = " "
-    line5[4] = "/"
+    line5[4] = "\\"
     render()
+    line9[1] = "\\"
+    line9[2] = "|"
+    line9[3] = "/"
+    line8[1] = "-"
+    line8[3] = "-"
+    line7[1] = "/"
+    line7[2] = "|"
+    line7[3] = "\\"
+    line5[4] = "0"
+    render()
+    line10[0] = "\\"
+    line10[2] = "|"
+    line10[4] = "/"
+    line8[0] = "-"
+    line8[4] = "-"
+    line6[0] = "/"
+    line6[2] = "|"
+    # line6[4] = "\\"
+    line6[3] = "\\"
+    line6[4] = "|"
+    line6[5] = "/"
+    line5[3] = "-"
+    line5[5] = "-"
+    line4[3] = "/"
+    line4[4] = "|"
+    line4[5] = "\\"
+    render()
+    line8[2] = " "
+    render()
+    line9[1] = " "
+    line9[2] = " "
+    line9[3] = " "
+    line8[1] = " "
+    line8[3] = " "
+    line7[1] = " "
+    line7[2] = " "
+    line7[3] = " "
     line5[4] = " "
-    line6[4] = "\\"
     render()
+    line10[0] = " "
+    line10[2] = " "
+    line10[4] = " "
+    line8[0] = " "
+    line8[4] = " "
+    line6[0] = " "
+    line6[2] = " "
     line6[4] = " "
-    line7[4] = "/"
-    render()
-    line7[4] = " "
-    line8[4] = "\\"
-    render()
-    line8[4] = "0"
+    line6[3] = " "
+    line6[4] = " "
+    line6[5] = " "
+    line5[3] = " "
+    line5[5] = " "
+    line4[3] = " "
+    line4[4] = " "
+    line4[5] = " "
     render()
